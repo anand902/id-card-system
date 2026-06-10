5643080965bdf167fe00635384a203bb5b9bb486
philip horo <56852632+anand902@users.noreply.github.com>
2026-06-11 03:38:54 +0530

    Add Firebase configuration to config.js
    Add config.js for Firebase configuration

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC74IUocux510KEoypM3jaTp9hpWjOzmko",
+  authDomain: "id-card-system-9afbd.firebaseapp.com",
+  databaseURL: "https://id-card-system-9afbd-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "id-card-system-9afbd",
+  storageBucket: "id-card-system-9afbd.firebasestorage.app",
+  messagingSenderId: "105103043325",
+  appId: "1:105103043325:web:f957a687573b8c8419ec94"
+};
