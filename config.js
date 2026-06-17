f12430d7ec832806add604219eec47c919dd1cbd
philip horo <56852632+anand902@users.noreply.github.com>
2026-06-17 16:49:08 +0530

    Change firebaseConfig to window.FIREBASE_CONFIG
    update firebase config system

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const firebaseConfig = {
+window.FIREBASE_CONFIG = {
   apiKey: "AIzaSyCAQS7dpNt-On5yEFVWX9SoW5-XJRFujqs",
   authDomain: "id-card-system-9afbd.firebaseapp.com",
   databaseURL: "https://id-card-system-9afbd-default-rtdb.asia-southeast1.firebasedatabase.app",
