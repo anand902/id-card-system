377468489a172975f500b96e5badcb7e20137f67
philip horo <56852632+anand902@users.noreply.github.com>
2026-06-18 07:50:04 +0530

    Rename FIREBASE_CONFIG to firebaseConfig

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-const FIREBASE_CONFIG = {
+const firebaseConfig = {
   apiKey: "AIzaSyCAQS7dpNt-On5yEFVWX9SoW5-XJRFujqs",
   authDomain: "id-card-system-9afbd.firebaseapp.com",
   databaseURL: "https://id-card-system-9afbd-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "id-card-system-9afbd",
   storageBucket: "id-card-system-9afbd.firebasestorage.app",
   messagingSenderId: "105103043325",
-  appId: "1:105103043325:web:f957a687573b8c8419ec94"
+  appId: "1:105103043325:web:41efaf8c100d4c8519ec94"
 };
